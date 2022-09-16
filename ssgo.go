50037e1f712fbee89e18375e6e1d8633819bd461
Artem Tanyhin <magic.cat.775@gmail.com>
2022-09-16 15:27:03 -0400

    file and directory creation

--- ssgo.go
+++ ssgo.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
 	flag "github.com/spf13/pflag"
 )
@@ -11,7 +14,7 @@ func main() {
 
 	var (
 		input string = ""
-		output string = "./dist"
+		output string = "dist"
 		help bool = false
 		version bool = false
 	)
@@ -24,7 +27,7 @@ func main() {
 	flag.Parse()
 
 	if len(os.Args) == 1 {
-		fmt.Println("Usage: ssgo -i [path]\nHelp: ssgo [-h | --help]")
+		fmt.Println("Usage: ssgo -i [path] -o [out path]\nHelp: ssgo [-h | --help]")
 		os.Exit(1)
 	} else {
 		if input != "" {
@@ -32,8 +35,9 @@ func main() {
 		} else if help {
 			fmt.Println("Basic usage: ssgo [flag] [value]")
 			fmt.Println("Flags:")
-			fmt.Println("\t[-i | --input] [path]      \t- Path to a .txt file OR a folder containing .txt files to be turned into HTML.")
-			fmt.Println("\t                           \t  By default, places generated HTML into ./dist")
+			fmt.Println("\t[-i | --input] [path]      \t- Path to a .txt file OR a folder containing .txt files to be turned into HTML")
+			fmt.Println("\t                           \t  For paths with spaces, please enclose them into double quotation marks, e.g. \"some path\"")
+			fmt.Println("\t                           \t  By default, places generated HTML into ./dist.")
 			fmt.Println("\t[-o | --output] [out path] \t- Optional. Additionaly changes the output path of generated HTML")
 			fmt.Println("\n\t[-v | --version]           \t- Display installed version of SSGo")
 			fmt.Println("\t[-h | --help]              \t- Display detailed help message")
@@ -49,5 +53,69 @@ func main() {
 
 func processInput(input string, output string) {
 	fmt.Println("Input: " + input)
-	fmt.Println("Input: " + output)
+
+	fileInfo, err := os.Stat(input)
+
+	if os.IsNotExist(err) {
+		fmt.Println("Error! No such file or directory!")
+		os.Exit(1)
+	}
+
+	output = strings.TrimSpace(output)
+	fmt.Println("Output: " + output)
+
+	if output == "" {
+		fmt.Println("Error! Output directory string is empty!")
+		os.Exit(1)
+	}
+
+	out, outerr := os.Stat(output)
+
+	if os.IsNotExist(outerr) {
+		// Output path doesn't exist
+		if err := os.Mkdir(output, os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	} else if !out.IsDir() {
+		// If path exists but is not a directory
+		log.Fatal("Error! Output path exists, but is not a directory!")
+	} else {
+		// If directory exists, delete it and re-create to get rid of the previous contents
+		fmt.Println("Removing previous compilations from the output folder...")
+		err := os.RemoveAll(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := os.Mkdir(output, os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if fileInfo.IsDir() {
+
+		fmt.Println("Generation from directory not implemented yet...")
+	
+	} else {
+
+		var (
+			basename string = fileInfo.Name()
+			ext string = filepath.Ext(basename)
+			name string = strings.TrimSuffix(basename, ext)
+		)
+
+		if ext != ".txt" {
+			fmt.Println("Error! Input file is not a .txt")
+			os.Exit(1)
+		}
+
+		fmt.Println("Title: " + name)
+
+		newFile, err := os.Create(output + "/" + name + ".html")
+		
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(newFile)
+		newFile.Close()
+	}
 }
